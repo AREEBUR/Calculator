07bb3c8745d8e2286ded813861e916c91543d72e
AREEBUR <81866078+AREEBUR@users.noreply.github.com>
2021-07-31 18:06:38 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+function getNumber(num){
+    console.log(num);
+    var result = document.getElementById("result");
+
+    //+= for catenation 
+    result.value +=  num;
+}
+
+function clr(){
+    
+    var result = document.getElementById("result"); 
+    result.value = "";
+}
+
+function getResult(){
+    var result = document.getElementById("result");
+    result.value = eval(result.value);
+    console.log(result.value);
+}
